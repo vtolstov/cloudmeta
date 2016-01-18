cd9d63ada71f1e9dc31c99b1126e7d25f36c499b
Vasiliy Tolstov <v.tolstov@selfip.ru>
2016-01-18 10:19:23 +0000

    fix string method
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- vendor/github.com/vtolstov/gopacket/layers/dhcpv4.go
+++ vendor/github.com/vtolstov/gopacket/layers/dhcpv4.go
@@ -276,7 +276,7 @@ func (p *DHCPv4) String() string {
 		}
 		options = strings.Join(lines, ", ")
 	}
-	return fmt.Sprintf("op: %s, htype: %v, hlen: %v, hopts: %v, xid: %#x, secs: %v, flags: %v, ciaddr: %v, yiaddr: %v, siaddr: %v, giaddr: %v, chaddr: %s, options: %s", p.Operation, p.HardwareType, p.HardwareLen, p.HardwareOpts, p.Xid, p.Secs, p.Flags, fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.ClientIP))), fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.YourIP))), fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.ServerIP))), fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.GatewayIP))), p.ClientHWAddr.String()[:17], options)
+	return fmt.Sprintf("op: %s, xid: %#x, ciaddr: %v, yiaddr: %v, siaddr: %v, giaddr: %v, chaddr: %s, options: %s", p.Operation, p.Xid, fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.ClientIP))), fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.YourIP))), fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.ServerIP))), fmt.Sprintf("%s", net.ParseIP(fmt.Sprintf("%s", p.GatewayIP))), p.ClientHWAddr.String()[:17], options)
 }
 
 func (p *DHCPv4) SetBroadcast(broadcast bool) {
